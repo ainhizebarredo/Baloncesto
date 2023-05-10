f2a259192417735fe1aee80e01e8a4fa11fe371b
ainhize barredo <ainhize.barredo@alumni.mondragon.edu>
2023-05-10 11:37:13 +0200

    quinto commit

--- arranque.R
+++ arranque.R
@@ -1,4 +1,7 @@
 library(plumber)
 getwd()
-r<-plumb('api.R')
-r$run(rport=8008)
+setwd('C:\\Users\\ainhi\\OneDrive\\Escritorio\\BDATA2\\ds1\\Baloncesto')
+r <- plumb("api.R")  # Where 'plumber.R' is the location of the file shown above
+#acaba de arranchar el fichero llamado plumber.R, a la espera de lo que le diga, con los servicios que tiene dentro
+r$run(port=8008) 
+
